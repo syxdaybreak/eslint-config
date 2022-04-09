0764895b42c15ad8a31530bfadcf012badef0766
Seiyial <seiyial@seiyial.xyz>
2022-04-10 03:39:13 +0800

    1.1.0

--- with-react/index.js
+++ with-react/index.js
@@ -4,7 +4,7 @@ module.exports = {
 		"es6": true
 	},
 	"extends": [
-		"plugin:react/recommended"
+		"plugin:react/jsx-runtime"
 	],
 	"globals": {
 		"Atomics": "readonly",
@@ -35,8 +35,12 @@ module.exports = {
 		"arrow-parens": ["warn", "always"],
 		"arrow-spacing": "error",
 		"block-spacing": ["warn", "always"],
-		"comma-dangle": "warn",
-		"comma-spacing": "warn",
+		"brace-style": "off",
+		"@typescript-eslint/brace-style": ["warn", "1tbs", { "allowSingleLine": true }],
+		"comma-dangle": "off",
+		"@typescript-eslint/comma-dangle": ["warn", "never"],
+		"comma-spacing": "off",
+		"@typescript-eslint/comma-spacing": ["warn"],
 		"computed-property-spacing": ["error", "never"],
 		"curly": 0,
 		"eol-last": "warn",
@@ -60,7 +64,7 @@ module.exports = {
 		"no-unused-expressions": 0,
 		"no-use-before-define": 0,
 		"no-useless-return": 0,
-		"@typescript-eslint/no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true }],
+		"@typescript-eslint/no-unused-expressions": ["warn", { "allowShortCircuit": true, "allowTernary": true }],
 		"no-unused-vars": 0, // ^
 		"object-curly-spacing": ["warn", "always"],
 		"padded-blocks": ["off", "always", { "allowSingleLineBlocks": true }],
@@ -68,24 +72,31 @@ module.exports = {
 		"prefer-const": "warn",
 		"quotes": ["warn", "single", { "allowTemplateLiterals": false }],
 		"react/jsx-boolean-value": ["warn", "never"],
+		"react/jsx-closing-bracket-location": ["warn", "tag-aligned"],
 		"react/jsx-curly-brace-presence": ["warn", { "props": "never", "children": "never" }],
 		"react/jsx-curly-spacing": ["warn", { "when": "never", "children": { "when": "always" } }],
 		"react/jsx-first-prop-new-line": ["warn", "multiline"],
 		"react/jsx-indent": ["warn", "tab"],
-		"react/jsx-max-props-per-line": ["warn", { "when": "multiline", "maximum": 1 }],
+		"react/jsx-max-props-per-line": ["warn", { "maximum": { "multi": 1, "single": 3 } }],
+		"react/jsx-sort-default-props": ["warn", { "ignoreCase": true } ],
 		"react/jsx-tag-spacing": "warn",
 		"react/no-access-state-in-setstate": "error",
+		"react/no-array-index-key": ["warn"],
 		"react/no-children-prop": "error",
 		"react/prefer-stateless-function": ["error", { "ignorePureComponents": false }],
 		"react/prop-types": "off",
+		"react/self-closing-comp": ["warn"],
+		"react/void-dom-elements-no-children": ["error"],
 		"react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-		"react-hooks/exhaustive-deps": 0,
+		"react-hooks/exhaustive-deps": ["off"],
 		"rest-spread-spacing": ["warn", "never"],
 		// "semi": ["warn", "never", { "beforeStatementContinuationChars": "any" }],
 		"semi": "off",
 		"@typescript-eslint/semi": ["warn", "never", { "beforeStatementContinuationChars": "any" }],
 		"space-before-function-paren": ["warn", "always"],
 		"space-infix-ops": ["warn", { "int32Hint": true }],
+		"@typescript-eslint/ban-ts-comment": ["error", { "ts-expect-error": false }],
+		"@typescript-eslint/member-delimiter-style": ["warn", { "multiline": { "delimiter": "comma", "requireLast": false }, "singleline": { "delimiter": "comma", "requireLast": false } }],
 		"@typescript-eslint/type-annotation-spacing": ["warn", {
 			"after": true,
 			"overrides": { "arrow": { "before": true, "after": true } }
